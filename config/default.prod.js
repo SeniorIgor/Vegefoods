6d9c834a587c3db85cd8b6ebfff1ff2e9b2240f9
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2021-01-25 18:32:04 +0300

    Commit (1.0.3) hosting

--- config/default.prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	PORT: 80,
-	JWT_SECRET: "some secret token",
-	MONGO_URI: "mongodb+srv://root:vlJ5bknBZb2YQk2q@cluster0.9sdgz.mongodb.net/app?retryWrites=true&w=majority",
-	BASE_URL: "http://localhost:80",
-	CATEGORY_ALL: "all"
-}
