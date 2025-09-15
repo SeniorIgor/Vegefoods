573eccb989c7fec74c8bd0cabd853a496545a0a0
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-09-15 20:06:48 +0600

    fix: removed outdated configurations

--- config/config.prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	PORT: process.env.PORT,
-	JWT_SECRET: process.env.JWT_SECRET,
-	MONGO_URI: process.env.MONGO_URI,
-	BASE_URL: process.env.BASE_URL,
-	CATEGORY_ALL: process.env.CATEGORY_ALL,
-}
